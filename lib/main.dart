d675ef1d79bb7eee5f9a5ffc7d64ac99528888dc
Udith <udithshalinda2@gmail.com>
2019-08-13 21:05:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:uhdmovies/ui/home.dart';
+
+
+void main() => runApp(
+  new MaterialApp(
+    title: 'movie app',
+    home:Home(),
+    )
+);
