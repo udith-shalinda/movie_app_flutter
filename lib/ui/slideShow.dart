c93b8f1a93c50596e59cbdf20ef6220f293127e8
Udith <udithshalinda2@gmail.com>
2019-08-14 17:10:48 +0530

    show movies in a grid view added

--- lib/ui/slideShow.dart
+++ lib/ui/slideShow.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class SlideShow extends StatefulWidget {
   @override
@@ -9,6 +10,12 @@ class SlideShow extends StatefulWidget {
 
 class _SlideShowState extends State<SlideShow> {
   int _current = 0;
+  final List<String> _listViewData = [
+    "A List View with many Text - Here's one!",
+    "A List View with many Text - Here's another!",
+    "A List View with many Text - Here's more!",
+    "A List View with many Text - Here's more!",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +63,7 @@ class _SlideShowState extends State<SlideShow> {
             aspectRatio: 2.0,
             viewportFraction: 0.8,
             enlargeCenterPage: true,
-            items: [1,2,3,4,5].map((i) {
+            items: _listViewData.map((i) {
               return Builder(
                 builder: (BuildContext context) {
                   return Container(
@@ -67,11 +74,48 @@ class _SlideShowState extends State<SlideShow> {
                       // shape: BoxShape.circle,
                       borderRadius: new BorderRadius.circular(10.0),
                     ),
-                    child: Text('text $i', style: TextStyle(fontSize: 16.0),)
-                  );
+                    child: Image.network('https://yourdubaiguide.com/wp-content/uploads/2019/03/Spider-Man-Far-From-Home-movie-release-date-showtimes-Dubai.jpg',
+                            fit: BoxFit.cover,),                  );
                 },
               );
             }).toList(),
+          ),
+           Padding(
+             padding: const EdgeInsets.only(top:10.0),
+             child: Row(
+               children: <Widget>[
+                 Text("New movies",
+                  style: TextStyle(
+                    fontFamily: 'Arvo',
+                    fontSize: 24,
+                    color: Colors.deepPurpleAccent,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+               ],
+             )
+           ),
+          Padding(padding: EdgeInsets.only(top: 20),),
+          Container(
+            height: 200,
+            child: GridView.count(
+              // crossAxisSpacing: 10.0,
+              // // padding: const EdgeInsets.all(20.0),
+              // shrinkWrap: true,
+              crossAxisCount: 2,
+              // Generate 100 widgets that display their index in the List.
+              children: List.generate(4, (index) {      //should return widget list
+                return Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      child: Image.network('https://yourdubaiguide.com/wp-content/uploads/2019/03/Spider-Man-Far-From-Home-movie-release-date-showtimes-Dubai.jpg',
+                              fit: BoxFit.cover,),
+                    ),
+                  ),
+                );
+              }),
+            )
           )
 
         ],
